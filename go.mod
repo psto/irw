a5cf8d706b3e2502058daa5284c1e251fc12ddf6
Piotr Stojanow <piotr.stojanow@gmail.com>
2026-04-16 22:21:04 +0200

    feat(tui): add terminal helpers

--- go.mod
+++ go.mod
@@ -11,7 +11,8 @@ require (
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/spf13/cobra v1.10.2 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
-	golang.org/x/sys v0.42.0 // indirect
+	golang.org/x/sys v0.43.0 // indirect
+	golang.org/x/term v0.42.0 // indirect
 	modernc.org/libc v1.70.0 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
 	modernc.org/memory v1.11.0 // indirect
---
+++ internal/tui/selector.go
@@ -0,0 +1,54 @@
+package tui
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"golang.org/x/term"
+)
+
+func LaunchFile(path string) error {
+	home, _ := os.UserHomeDir()
+	launchPath := home + "/.local/bin/launch"
+	cmd := exec.Command(launchPath, path)
+	return cmd.Run()
+}
+
+func RunZk(args ...string) ([]byte, error) {
+	cmd := exec.Command("zk", args...)
+	return cmd.Output()
+}
+
+func ReadKey() (rune, error) {
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		return 0, err
+	}
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+
+	buf := make([]byte, 1)
+	_, err = os.Stdin.Read(buf)
+	return rune(buf[0]), err
+}
+
+func GetHomeDir() (string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return home, nil
+}
+
+func AbsPath(input string) (string, error) {
+	return filepath.Abs(input)
+}
+
+func FileExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+func BaseName(path string) string {
+	return filepath.Base(path)
+}
